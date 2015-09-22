0cc0f0538d6b64b6c4138b12e827f8209935f354
mahpah <mahpahh@gmail.com>
2015-09-22 17:31:31 +0700

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
 import TheClass from './TheClass.js';
 import DomUtils from './DomUtils.js';
-console.log(DomUtils);
 
-document.addEventListener('DOMContentLoaded', e => {
-  let ps = document.querySelectorAll('p');
-  setTimeout(() => {
-    DomUtils.toggleClass('test', ps);
-  }, 2000);
-});
+// document.addEventListener('DOMContentLoaded', e => {
+//   let ps = document.querySelectorAll('p');
+//   setTimeout(() => {
+//     DomUtils.toggleClass('test', ps);
+//   }, 2000);
+// });
